1571603022840e75b22205c7d094cc06270834e8
AndresFZapataR <38545379+AndresFZapataR@users.noreply.github.com>
2018-11-26 11:20:34 -0500

    Update greedyExample.cpp

--- greedyExample.cpp
+++ greedyExample.cpp
@@ -1,61 +1,48 @@
 #include<iostream>
-
 #include<vector>
-
 
 using namespace std;
 
-
-
 bool palitos(vector<size_t> pal, size_t size);
-
 size_t select(vector<size_t> pal, size_t size);
 
-
-
 int main(){
-
-	vector<size_t> arr = {7,5,3,2,1};
-
-	cout<<palitos(arr,16)<<endl;
-
+	vector<size_t> arr = {7,5,3};
+	cout<<"Tenemos baras de este tamaño: "<<endl;
+	for(size_t i = 0; i < arr.size(); i++){
+		cout<<">"<< arr[i]<<endl;
+	}
+	size_t tam = 15;
+	cout<<"Queremos armar una bara de tamaño "<<tam<<" apartir de las anteriores:"<<endl;
+	bool res = palitos(arr, tam);
+	if(res){
+		cout<<"Si se puede armar!"<<endl;
+	}else{
+		cout<<"No se puede armar"<<endl;
+	}
 	return 0;
-
 }
 
-
-
 bool palitos(vector<size_t> pal,size_t size){
-
-	if(select(pal,size) == 0){
-
+	size_t sel = select(pal,size);
+	if(sel == 0){
+		cout<<"No tenemos una bara de ese tamaño"<<endl;
 		return false;
-
 	}
-
-	if(select(pal,size) == size){
-
+	if(sel == size){
 		return true;
-
 	}else{
-
-		return palitos(pal, size - select(pal,size));
-
+		cout<<"Hace falta "<< size - sel<<endl;
+		return palitos(pal, size - sel);
 	}
-
-
+}
 
 size_t select(vector<size_t> pal, size_t size){
-
 	for(size_t i = 0; i < pal.size(); i++){
-
 		if(pal[i] <= size){
-
+			cout<<"Usamos una bara de tamaño: "<<pal[i]<<endl;
 			return pal[i];
-
 		}
-
 	}
-
 	return 0;
 }
